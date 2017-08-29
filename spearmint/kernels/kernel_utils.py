7bec3e4909d4f5747e005a99b3155cf7c87357a6
Jaume Coll-Font <jcollfont@gmail.com>
2017-08-29 13:25:01 -0400

    modified the references to weave and eliminated their dependence to scpipy

--- spearmint/kernels/kernel_utils.py
+++ spearmint/kernels/kernel_utils.py
@@ -184,25 +184,25 @@
 
 
 import numpy as np
-import scipy.weave
+import weave
 from scipy.spatial.distance import cdist
 
 def dist2(ls, x1, x2=None):
     # Assumes NxD and MxD matrices.
     # Compute the squared distance matrix, given length scales.
-    
+
     if x2 is None:
         # Find distance with self for x1.
 
         # Rescale.
-        xx1 = x1 / ls        
+        xx1 = x1 / ls
         xx2 = xx1
 
     else:
         # Rescale.
         xx1 = x1 / ls
         xx2 = x2 / ls
-    
+
     r2 = cdist(xx1,xx2,'sqeuclidean')
 
     return r2
@@ -210,11 +210,11 @@ def dist2(ls, x1, x2=None):
 def grad_dist2(ls, x1, x2=None):
     if x2 is None:
         x2 = x1
-        
+
     # Rescale.
     x1 = x1 / ls
     x2 = x2 / ls
-    
+
     N = x1.shape[0]
     M = x2.shape[0]
     D = x1.shape[1]
@@ -228,8 +228,8 @@ def grad_dist2(ls, x1, x2=None):
           gX(i,j,d) = (2/ls(d))*(x1(i,d) - x2(j,d));
     """
     try:
-        scipy.weave.inline(code, ['x1','x2','gX','ls','M','N','D'], \
-                           type_converters=scipy.weave.converters.blitz, \
+        weave.inline(code, ['x1','x2','gX','ls','M','N','D'], \
+                           type_converters=weave.converters.blitz, \
                            compiler='gcc')
     except:
     # The C code weave above is 10x faster than this:
@@ -266,6 +266,3 @@ def dist_Mahalanobis(U, x1, x2=None):
 
 #         return func(cls_instance, *new_args)
 #     return inner
-    
-
-
