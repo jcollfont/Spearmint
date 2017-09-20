e5633e4ddca97db3ccf56a08c92336853d3d7f5a
Jaume Coll-Font <jcollfont@gmail.com>
2017-09-20 12:28:15 -0400

    loading moist locally

--- examples/MINSTexample/MINST_keras_remote.py
+++ examples/MINSTexample/MINST_keras_remote.py
@@ -29,7 +29,8 @@ def setTestandTrain():
     num_classes = 10
 
     # the data, shuffled and split between train and test sets
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+#    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    (x_train, y_train), (x_test, y_test) = mnist.load_data(path="/home/jcollfont/Documents/Research/Spearmint/examples/MINSTexample/mnist.npz")
 
     x_train = x_train.reshape(60000, 784)
     x_test = x_test.reshape(10000, 784)
